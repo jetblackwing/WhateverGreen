9092f17bb2080fa3d30fba4bdec76eeb66028a66
Amal JK <amaljk80@gmail.com>
2024-03-09 15:59:13 +0530

    Remove graphics libraries other than TGL

--- WhateverGreen/kern_igfx_kexts.hpp
+++ WhateverGreen/kern_igfx_kexts.hpp
@@ -11,23 +11,7 @@
 
 #include <Headers/kern_patcher.hpp>
 
-extern KernelPatcher::KextInfo kextIntelHD;
-extern KernelPatcher::KextInfo kextIntelHDFb;
-extern KernelPatcher::KextInfo kextIntelHD3000;
-extern KernelPatcher::KextInfo kextIntelSNBFb;
-extern KernelPatcher::KextInfo kextIntelHD4000;
-extern KernelPatcher::KextInfo kextIntelCapriFb;
-extern KernelPatcher::KextInfo kextIntelHD5000;
-extern KernelPatcher::KextInfo kextIntelAzulFb;
-extern KernelPatcher::KextInfo kextIntelBDW;
-extern KernelPatcher::KextInfo kextIntelBDWFb;
-extern KernelPatcher::KextInfo kextIntelSKL;
-extern KernelPatcher::KextInfo kextIntelSKLFb;
-extern KernelPatcher::KextInfo kextIntelKBL;
-extern KernelPatcher::KextInfo kextIntelKBLFb;
-extern KernelPatcher::KextInfo kextIntelCFLFb;
-extern KernelPatcher::KextInfo kextIntelCNL;
-extern KernelPatcher::KextInfo kextIntelCNLFb;
+
 extern KernelPatcher::KextInfo kextIntelICL;
 extern KernelPatcher::KextInfo kextIntelICLLPFb;
 extern KernelPatcher::KextInfo kextIntelICLHPFb;
